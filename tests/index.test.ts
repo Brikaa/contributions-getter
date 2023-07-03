7a27aade26b2c44f5119bb412c1dc2f284f365cf
Omar Brikaa <brikaaomar@gmail.com>
2023-07-03 14:27:17 +0300

    More unit tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -20,8 +20,19 @@ describe('getContributions', () => {
     );
   });
 
-  it('returns the contributions when the auth token and data are correct', async () => {
+  it('returns no undefined property values in each contribution object', async () => {
     const contributions = await getContributions(authToken, 'brikaa');
+
+    contributions.forEach((contribution) => {
+      const contributionClone: any = { ...contribution };
+      for (const key in contributionClone) expect(contributionClone[key]).toBeDefined();
+
+      contribution.repos.forEach((r) => {
+        const repoClone: any = { ...r };
+        for (const key in repoClone) expect(repoClone[key]).toBeDefined();
+      });
+    });
+
     console.log(jsonStringify(contributions));
   });
 
@@ -31,20 +42,36 @@ describe('getContributions', () => {
   });
 
   it('does not accept a monthsInterval that is more than 12', async () => {
-    await expect(() => getContributions(authToken, 'brikaa', { monthsInterval: 13 })).rejects.toThrow(
-      InvalidConfigException
-    );
+    await expect(() =>
+      getContributions(authToken, 'brikaa', { monthsInterval: 13 })
+    ).rejects.toThrow(InvalidConfigException);
   });
 
   it('does not accept a monthsInterval that is less than 1', async () => {
-    await expect(() => getContributions(authToken, 'brikaa', { monthsInterval: 0 })).rejects.toThrow(
-      InvalidConfigException
-    );
+    await expect(() =>
+      getContributions(authToken, 'brikaa', { monthsInterval: 0 })
+    ).rejects.toThrow(InvalidConfigException);
   });
 
   it('does not accept a decimal monthsInterval', async () => {
-    await expect(() => getContributions(authToken, 'brikaa', { monthsInterval: 3.4 })).rejects.toThrow(
-      InvalidConfigException
-    );
+    await expect(() =>
+      getContributions(authToken, 'brikaa', { monthsInterval: 3.4 })
+    ).rejects.toThrow(InvalidConfigException);
   });
+
+  it('divides the periods according to monthsInterval correctly', async () => {
+    const monthsInterval = 11;
+    const contributions = await getContributions(authToken, 'brikaa', { monthsInterval });
+    contributions.forEach((c) => {
+      const expectedStartDate = c.endDate;
+      expectedStartDate.setMonth(expectedStartDate.getMonth() - monthsInterval);
+      expect(expectedStartDate).toStrictEqual(c.startDate);
+    });
+  });
+});
+
+describe.skip('getContributions intensive operations', () => {
+  it('does not get rate-limited with around 60 requests', async () => {
+    await getContributions(authToken, 'brikaa', { monthsInterval: 1 });
+  }, 20000);
 });
